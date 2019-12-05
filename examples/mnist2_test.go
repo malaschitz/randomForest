3dfa93dc2775b8413299309fd8bb3053bd1a83a1
Richard Malaschitz <malaschitz@gmail.com>
2019-12-05 17:31:44 +0100

    goreportcard fixing

--- examples/mnist2_test.go
+++ examples/mnist2_test.go
@@ -94,6 +94,9 @@ func writeImage(data []byte, name string) {
 	img := image.NewGray(image.Rect(0, 0, 28, 28))
 	img.Pix = data
 	out, err := os.Create("./" + name + ".png")
+	if err != nil {
+		log.Fatal(err)
+	}
 	err = png.Encode(out, img)
 	if err != nil {
 		log.Fatal(err)
